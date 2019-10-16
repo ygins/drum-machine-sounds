03da2df028450a2a9b7a9e65af3fdafa55a6ce67
Yonatan <yginsburg26@gmail.com>
2019-10-16 15:33:24 -0400

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express=require("express");
 const fs=require("fs");
 const path=require("path");
+const cors=require("cors");
 const app=express();
+
+
+
+app.use(cors());
 
 app.get("/paths", (req, res)=>{
   const responseObj={};
