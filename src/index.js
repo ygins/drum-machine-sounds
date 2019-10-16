b3ff5ff153f5fd4fdb280b21de4a19a06dad82f3
Yonatan <yginsburg26@gmail.com>
2019-10-16 15:22:48 -0400

    Removed cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const express=require("express");
 const fs=require("fs");
 const path=require("path");
-const cors=require("cors");
 const app=express();
-
-
-
-app.use(cors());
 
 app.get("/paths", (req, res)=>{
   const responseObj={};
